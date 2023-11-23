1f22021115f7c3778a522476653031c797595c4d
Fazulhaq Amin <amin.jawan@yahoo.com>
2023-11-23 10:06:28 +0430

    SignIn and SignUp components completed and works.

---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,15 @@
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+
+const Athentication = () => {
+
+    return(
+        <div>
+            <h1>Sign In Page</h1>
+            <SignInForm />
+            <SignUpForm />
+        </div>
+    );
+}
+
+export default Athentication;
