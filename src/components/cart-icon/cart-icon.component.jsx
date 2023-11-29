5f573d3ca4e08047c0ad7e925abd2ba3b5399169
Fazulhaq Amin <amin.jawan@yahoo.com>
2023-11-29 12:23:30 +0430

    Add item to cart functinality completed

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,10 +7,10 @@ import { CartContext } from "../../contexts/cart.context";
 const CartIcon = () => {
     const { isCartOpen, setIsCartOpen } = useContext(CartContext);
 
-    const toogleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
     return(
-        <div className="cart-icon-container" onClick={toogleIsCartOpen}>
+        <div className="cart-icon-container" onClick={toggleIsCartOpen}>
             <ShoppingIcon className="shopping-icon" />
             <span className="item-count">0</span>
         </div>
