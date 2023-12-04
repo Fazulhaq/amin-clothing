4e7567e70627d5f2827d98e1268497020f0781cf
Fazulhaq Amin <amin.jawan@yahoo.com>
2023-12-04 14:29:48 +0430

    Links added for different categories

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./directory-item.styles.scss";
 
 const DirectoryItem = ({category}) => {
@@ -13,8 +14,10 @@ const DirectoryItem = ({category}) => {
             }}
           />
           <div className="body">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
+            <h2>{title.toUpperCase()}</h2>
+            <Link className="title" to={`shop/${title}`}>
+              <p>Shop Now</p>
+            </Link>
           </div>
         </div>
     );
