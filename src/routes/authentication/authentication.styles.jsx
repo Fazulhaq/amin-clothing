3792da6fa796b1ff5a319cf31aeed2852342a73f
Fazulhaq Amin <amin.jawan@yahoo.com>
2023-12-05 12:47:13 +0430

    Routes scss changed to styled-components of jsx

--- src/routes/authentication/authentication.styles.jsx
+++ src/routes/authentication/authentication.styles.jsx
@@ -1,6 +1,8 @@
-.authentication-container {
+import styled from "styled-components";
+
+export const AuthenticationContainer = styled.div`
     display: flex;
     justify-content: space-between;
     width: 70%;
     margin: 30px auto;
-}
+`;
