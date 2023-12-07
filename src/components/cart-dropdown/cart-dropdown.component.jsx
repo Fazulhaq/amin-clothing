bd301d6420e68743131bac3425702084cc122512
Fazulhaq Amin <amin.jawan@yahoo.com>
2023-12-07 09:21:04 +0430

    CartDropdown comp styles changed to styled(jsx)

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,7 +4,7 @@ import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { CartContext } from "../../contexts/cart.context";
 
-import "./cart-dropdown.styles.scss";
+import { CartDropdownContainer, CartItems, EmptyMessage} from "./cart-dropdown.styles";
 
 
 const CartDropdown = () => {
@@ -16,14 +16,18 @@ const CartDropdown = () => {
         setIsCartOpen(!isCartOpen);
     }
     return(
-        <div className="cart-dropdown-container">
-            <div className="cart-items" >
-                {cartItems.map(item => 
+        <CartDropdownContainer>
+            <CartItems>
+                {
+                cartItems.length ? (cartItems.map(item => 
                 <CartItem key={item.id} cartItem={item}/>
-                )}
-            </div>
+                )) : (
+                    <EmptyMessage>Your cart is empty</EmptyMessage>
+                )
+                }
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
-        </div>
+        </CartDropdownContainer>
     )
 }
 
