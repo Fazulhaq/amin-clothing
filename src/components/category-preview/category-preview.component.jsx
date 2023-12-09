a363c61c7e1cd51cf293797cae68c3112d89393d
Fazulhaq Amin <amin.jawan@yahoo.com>
2023-12-09 10:59:11 +0430

    CategoryPreview styles changed to styled-component

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,17 +1,16 @@
-import { Link } from "react-router-dom";
 
 import ProductCard from "../product-card/product-card.component";
 
-import "./category-preview.styles.scss";
+import { CategoryPreviewContainer, LinkTitle, Preview } from "./category-preview.styles";
 
 const CategoryPreview = ({ title, products }) => {
 
     return(
-        <div className="category-preview-container">
+        <CategoryPreviewContainer>
             <h2>
-                <Link className="title" to={title}>{ title.toUpperCase() }</Link>
+                <LinkTitle to={title}>{ title.toUpperCase() }</LinkTitle>
             </h2>
-            <div className="preview">
+            <Preview>
             {
                 products
                 .filter((_, indx) => indx < 4)
@@ -19,8 +18,8 @@ const CategoryPreview = ({ title, products }) => {
                     <ProductCard key={product.id} product={product} />
                 )
             }
-            </div>
-        </div>
+            </Preview>
+        </CategoryPreviewContainer>
     )
 }
 
