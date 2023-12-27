e6aa6eff8a324d779e30bf31c6d91cf1c37dde2b
Fazulhaq Amin <amin.jawan@yahoo.com>
2023-12-27 14:51:38 +0430

    redux-saga middleware used for category data

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,14 +2,14 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 
-import { fetchCategoriesAsync } from "../../store/categories/category.action";
+import { fetchCategoriesStart } from "../../store/categories/category.action";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 const Shop = () => {
     const dispatch = useDispatch();
     useEffect(() => {
-            dispatch(fetchCategoriesAsync());
-        },[]);
+            dispatch(fetchCategoriesStart());
+        },[dispatch]);
     return(
         <Routes>
             <Route index element={ <CategoriesPreview /> } />
--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,12 @@ import {
   compose,
   legacy_createStore as createStore,
 } from "redux";
+import logger from "redux-logger";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { thunk } from "redux-thunk";
-
+import createSagaMiddleware from "redux-saga";
 import { rootReducer } from "./root-reducer";
-
-import logger from "redux-logger";
+import { rootSaga } from "./root-saga";
 
 const persistConfig = {
   key: "root",
@@ -17,11 +16,13 @@ const persistConfig = {
   whitelist: ["cart"],
 };
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
-  thunk,
+  sagaMiddleware,
 ].filter(Boolean);
 
 const composeEnhancer =
@@ -33,5 +34,7 @@ const composeEnhancer =
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, composedEnhancers);
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
