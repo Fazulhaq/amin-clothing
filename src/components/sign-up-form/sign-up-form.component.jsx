1344d2a605ccd4f792a5233f907fbf35d73cd826
Fazulhaq Amin <amin.jawan@yahoo.com>
2024-01-02 11:42:49 +0430

    signUp converted to userSaga

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { createAuthUserWithEmailAndPassword, createUserDocumentFrom } from "../../utils/firebase/firebase.utils";
-import FormInput from "../form-input/form-input.component";
+import { useDispatch } from "react-redux";
 import Button from "../button/button.component";
+import FormInput from "../form-input/form-input.component";
 
+import { signUpStart } from "../../store/user/user.action";
 import { SignUpConainer } from "./sign-up-form.styles";
 
 const defaultFormFields = {
@@ -16,6 +17,7 @@ const SignUpForm = () => {
 
     const [formFields, setFormFields] = useState(defaultFormFields);
     const {displayName, email, password, confirmPassword} = formFields;
+    const dispatch = useDispatch();
 
     const resetFields = () => {
         setFormFields(defaultFormFields);
@@ -28,8 +30,7 @@ const SignUpForm = () => {
             return;
         }
         try{
-            const { user } = await createAuthUserWithEmailAndPassword(email, password);
-            await createUserDocumentFrom(user, { displayName });
+            dispatch(signUpStart(email, password, displayName));
             resetFields();
         }catch(error){
             if(error.code === "auth/weak-password"){
--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -2,9 +2,15 @@ import { all, call, put, takeLatest } from "redux-saga/effects";
 
 import { USER_ACTION_TYPES } from "./user.types";
 
-import { signInFailed, signInSuccess } from "./user.action";
+import {
+  signInFailed,
+  signInSuccess,
+  signUpFailed,
+  signUpSuccess,
+} from "./user.action";
 
 import {
+  createAuthUserWithEmailAndPassword,
   createUserDocumentFrom,
   getCurrentUser,
   signInAuthUserWithEmailAndPassword,
@@ -56,6 +62,23 @@ export function* isUserAuthenticated() {
   }
 }
 
+export function* signUp({ payload: { email, password, displayName } }) {
+  try {
+    const { user } = yield call(
+      createAuthUserWithEmailAndPassword,
+      email,
+      password
+    );
+    yield put(signUpSuccess(user, { displayName }));
+  } catch (error) {
+    yield put(signUpFailed(error));
+  }
+}
+
+export function* singInAfterSignUp({ payload: { user, additionalDetails } }) {
+  yield call(getSnapshotFromUserAuth, user, additionalDetails);
+}
+
 export function* onGoogleSignInStart() {
   yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -68,10 +91,20 @@ export function* onEmailSignInStart() {
   yield takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail);
 }
 
+export function* onSignUpStart() {
+  yield takeLatest(USER_ACTION_TYPES.SIGN_UP_START, signUp);
+}
+
+export function* onSignUpSuccess() {
+  yield takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, singInAfterSignUp);
+}
+
 export function* userSagas() {
   yield all([
     call(onCheckUserSession),
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
+    call(onSignUpStart),
+    call(onSignUpSuccess),
   ]);
 }
