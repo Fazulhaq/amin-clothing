86e5b11399f222a322de93fc02423d6ac626b4eb
Fazulhaq Amin <amin.jawan@yahoo.com>
2024-01-06 12:44:10 +0430

    Setting up payment form for the project(x)

--- src/components/checkout/checkout.component.jsx
+++ src/components/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
 import CheckoutItem from "../checkout-item/checkout-item.component";
+import PaymentForm from "../payment-form/payment-form.component";
 import { CheckoutContainer, CheckoutHeader, HeaderBlock } from "./checkout.styles";
 
 const Checkout = () => {
@@ -30,6 +31,7 @@ const Checkout = () => {
                     <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
                     )}
             <span className="total">Total: ${cartTotal}</span>
+            <PaymentForm />
         </CheckoutContainer>
     )
 }
