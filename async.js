6c6cc2020dace557a480fdbcc908f587d96ed5d5
Fazulhaq Amin <amin.jawan@yahoo.com>
2024-01-14 21:54:29 +0430

    async & await explained inside this code

--- async.js
+++ async.js
@@ -1,3 +1,49 @@
+// async and await explained
+
+// 1- using fetch with Promise base code
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((users) => {
+    const firstUser = users[0];
+    console.log("First user data:");
+    console.log(firstUser);
+    return fetch(
+      "https://jsonplaceholder.typicode.com/posts?userId=" + firstUser.id
+    );
+  })
+  .then((response) => response.json())
+  .then((posts) => {
+    console.log("First user posts:");
+    console.log(posts);
+  })
+  .catch((error) => console.log(error));
+
+// 2 - using async and await method
+
+const myAsyncFunction = async () => {
+  try {
+    const usersResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    const users = await usersResponse.json();
+    const secondUser = users[1];
+    console.log("Second user data:");
+    console.log(secondUser);
+    const postsResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/posts?userId=" + secondUser.id
+    );
+    const posts = await postsResponse.json();
+    console.log("Second user posts: ");
+    console.log(posts);
+  } catch (error) {
+    console.log("There was an error as following: ");
+    console.log(error);
+  }
+};
+
+myAsyncFunction();
+
 // caching explained
 
 // function addTo80(n) {
@@ -10,26 +56,26 @@
 // console.log(addTo80(5));
 // console.log(addTo80(5));
 
-let cache = {};
+// let cache = {};
 
-function addTo80Caching(n) {
-  if (n in cache) {
-    return cache[n];
-  } else {
-    console.log("long time.");
-    cache[n] = n + 80;
-    return cache[n];
-  }
-}
+// function addTo80Caching(n) {
+//   if (n in cache) {
+//     return cache[n];
+//   } else {
+//     console.log("long time.");
+//     cache[n] = n + 80;
+//     return cache[n];
+//   }
+// }
 
-console.log(addTo80Caching(8));
+// console.log(addTo80Caching(8));
 
-console.log("first: long time message will be shown");
+// console.log("first: long time message will be shown");
 
-console.log(addTo80Caching(10));
-console.log(addTo80Caching(10));
-console.log(addTo80Caching(10));
-console.log(addTo80Caching(10));
+// console.log("1", addTo80Caching(10));
+// console.log("2", addTo80Caching(10));
+// console.log("3", addTo80Caching(10));
+// console.log("4", addTo80Caching(10));
 
 // const multiply = (a, b) => a * b;
 // const res = multiply(4, 5);
