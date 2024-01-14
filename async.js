b224b209fbe7225a8ba26040b5f4afca24adc0ba
Fazulhaq Amin <amin.jawan@yahoo.com>
2024-01-14 11:06:31 +0430

    ES6 classes in JavaScript

--- async.js
+++ async.js
@@ -1,7 +1,21 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json))
-  .catch((error) => console.log("Failed to get data"));
+// ES6 classes in JavaScript
+class Elf {
+  constructor(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+  }
+  attack() {
+    console.log(this.name + " attacked with " + this.weapon);
+  }
+}
+
+const ahmad = new Elf("Ahmad", "stone");
+console.log(ahmad.attack());
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json))
+//   .catch((error) => console.log("Failed to get data"));
 
 // const myPromise = new Promise((resolve, reject) => {
 //   if (true) {
