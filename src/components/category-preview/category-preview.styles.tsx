06542868371a07520a98a250ccff25018b16c34e
Fazulhaq Amin <amin.jawan@yahoo.com>
2024-01-20 14:12:12 +0430

    @media queries implemented on shop page

--- src/components/category-preview/category-preview.styles.tsx
+++ src/components/category-preview/category-preview.styles.tsx
@@ -5,6 +5,11 @@ export const CategoryPreviewContainer = styled.div`
     display: flex;
     flex-direction: column;
     margin-bottom: 30px;
+
+    @media screen and (max-width: 800px) {
+        align-items: center;
+    }
+
 `;
 
 export const LinkTitle = styled(Link)`
@@ -17,4 +22,10 @@ export const Preview = styled.div`
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     column-gap: 20px;
+
+    @media screen and (max-width: 800px) {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 15px;
+    }
 `;
